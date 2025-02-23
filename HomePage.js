ab3d411966eda2316203d86eb06fd022afa01907
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-02-23 16:51:06 -0500

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,14 @@
+// In src/components/HomePage.js
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Language Learning App</h1>
+      <p>Start learning a new language today!</p>
+    </div>
+  );
+}
+
+export default HomePage;  // Make sure to use default export
+
