6a9dd4a916eedc61e5f8cb74095361312c2833b3
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-02-25 16:51:33 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";  // No BrowserRouter here
 import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import LessonPage from "./components/LessonPage"; // Import Lesson Page
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LoginPage />} />  {/* Default login route */}
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/lesson" element={<LessonPage />} />
     </Routes>
   );
 }
