5b22b14888bf30bdfb78ce475d1240588c71964d
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-02-28 17:27:02 -0500

    Added Function to store UserName

--- SignupPage.js
+++ SignupPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignupPage.css";
-
+ 
 function SignupPage() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
