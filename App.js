8f56063030c06874300cfe1b171632cac4220400
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-03-11 17:31:46 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import LessonPage from "./components/LessonPage"; // Import Lesson Page
+import SurveyPage from "./components/SurveyPage";
+import SettingsPage from "./components/SettingsPage"; 
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Route path="/home" element={<HomePage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/lesson" element={<LessonPage />} />
+      <Route path="/survey" element={<SurveyPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
     </Routes>
   );
 }
