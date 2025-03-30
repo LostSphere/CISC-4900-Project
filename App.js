fbf7d77575218b92150e74e0a6a3d4317544d150
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-03-30 16:12:57 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";  // No BrowserRouter here
+import { Routes, Route } from "react-router-dom";  
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
-import LessonPage from "./components/LessonPage"; // Import Lesson Page
-import SurveyPage from "./components/SurveyPage";
+import LessonPage from "./components/LessonPage"; 
 import SettingsPage from "./components/SettingsPage"; 
 import SpanishStory from "./spanishStory/SpanishStory";
-
+import TestYourself from "./components/TestYourself";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />  {/* Default login route */}
+      <Route path="/" element={<LoginPage />} /> 
       <Route path="/home" element={<HomePage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/lesson" element={<LessonPage />} />
-      <Route path="/survey" element={<SurveyPage />} />
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="/spanish-story" element={<SpanishStory />} />
+      <Route path="/test-yourself" element={<TestYourself />} /> 
     </Routes>
   );
 }
