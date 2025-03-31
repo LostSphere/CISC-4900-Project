5cd8c71ffcd58d4fc2fc17cb47e45d8181aa930f
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-03-31 16:48:57 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import LessonPage from "./components/LessonPage"; 
 import SettingsPage from "./components/SettingsPage"; 
-import SpanishStory from "./spanishStory/SpanishStory";
 import TestYourself from "./components/TestYourself";
+import SpanishStory from "./AdventureLessons/spanishStory/SpanishStory";
+import IntroToFrench from "./IntroLessons/IntroFrench/IntroToFrench";
+import IntroToSpanish from "./IntroLessons/IntroSpanish/IntroToSpanish";
+import IntroToJapanese from "./IntroLessons/IntroJapanese/IntroToJapanese";
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="/spanish-story" element={<SpanishStory />} />
       <Route path="/test-yourself" element={<TestYourself />} /> 
+      <Route path="/intro-to-french" element={<IntroToFrench />} />
+      <Route path="/intro-to-spanish" element={<IntroToSpanish />} />
+      <Route path="/intro-to-japanese" element={<IntroToJapanese />} />
     </Routes>
   );
 }
