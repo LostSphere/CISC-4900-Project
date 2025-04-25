4784f5923f50a71c73208f9288dd2891b6854902
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-04-25 16:04:47 -0400

    Update SpanishStory.js

--- src/AdventureLessons/SpanishFiesta/SpanishStory.js
+++ src/AdventureLessons/SpanishFiesta/SpanishStory.js
@@ -83,17 +83,27 @@ function SpanishStory() {
   };  
 
   const handleQuizAnswer = (selectedOption) => {
-    if (selectedOption === quizData[currentQuestionIndex].answer) {
-      setScore(score + 1);
+    let updatedScore = score;
+  
+    if (quizData[currentQuestionIndex].answer.includes(selectedOption)) {
+      updatedScore += 1;
+      setScore(updatedScore);
     }
-    
+  
     if (currentQuestionIndex < quizData.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
       setQuizCompleted(true);
-
+  
       const completionSound = new Audio("/sounds/quiz_complete.mp3");
       completionSound.play();
+  
+      localStorage.setItem("quizProgress_SpanishFiesta", JSON.stringify({ score: updatedScore }));
+  
+      const highestScore = parseInt(localStorage.getItem("highestScore_SpanishFiesta"), 10) || 0;
+      if (updatedScore > highestScore) {
+        localStorage.setItem("highestScore_SpanishFiesta", updatedScore);
+      }
     }
   };
 
