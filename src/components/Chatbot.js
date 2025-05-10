d87897a2da7d3b160b9a567e73ec84c820c8bf8b
WeiChengZhu <159456360+LostSphere@users.noreply.github.com>
2025-05-09 20:47:40 -0400

    Update Chatbot.js

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -8,25 +8,57 @@ const Chatbot = () => {
     { text: "Hello! How can I help you today? 😃", fromUser: false },
   ]);
   const [isOpen, setIsOpen] = useState(false);
+  const [botIsTyping, setBotIsTyping] = useState(false);
   
   const messagesEndRef = useRef(null);
   const location = useLocation();
 
   const responseOptionsLookup = {
+    
+    // Intro courses
     "/intro-to-english": chatData.introEnglishResponses,
     "/intro-to-french": chatData.introFrenchResponses,
     "/intro-to-spanish": chatData.introSpanishResponses,
     "/intro-to-japanese": chatData.introJapaneseResponses,
     "/intro-to-mandarin": chatData.introMandarinResponses,
     "/intro-to-italian": chatData.introItalianResponses,
-    "/intro-to-german": chatData.introGermanResponses
+    "/intro-to-german": chatData.introGermanResponses,
+
+    // Intermediate Courses
+    "/intermediate-to-english": chatData.intermediateEnglishResponses,
+    "/intermediate-to-french": chatData.intermediateFrenchResponses,
+    "/intermediate-to-spanish": chatData.intermediateSpanishResponses,
+    "/intermediate-to-japanese": chatData.intermediateJapaneseResponses,
+    "/intermediate-to-mandarin": chatData.intermediateMandarinResponses,
+    "/intermediate-to-italian": chatData.intermediateItalianResponses,
+    "/intermediate-to-german": chatData.intermediateGermanResponses,
+
+    // Advanced Courses
+    "/advanced-to-english": chatData.advancedEnglishResponses,
+    "/advanced-to-french": chatData.advancedFrenchResponses,
+    "/advanced-to-spanish": chatData.advancedSpanishResponses,
+    "/advanced-to-japanese": chatData.advancedJapaneseResponses,
+    "/advanced-to-mandarin": chatData.advancedMandarinResponses,
+    "/advanced-to-italian": chatData.advancedItalianResponses,
+    "/advanced-to-german": chatData.advancedGermanResponses,
+
+    // Cultural Adventures
+    "/spanish-story": chatData.SpanishStoryResponses,
+    "/japanese-story": chatData.JapaneseStoryResponses,
+    "/french-story": chatData.FrenchStoryResponses,
+    "/mandarin-story": chatData.MandarinStoryResponses,
+    "/italian-story": chatData.ItalianStoryResponses,
+    "/german-story": chatData.GermanStoryResponses,
+    "/literature-story": chatData.LiteratureStoryResponses,
+    "/debating-story": chatData.DebatingStoryResponses,
+    "/business-story": chatData.BusinessStoryResponses,
   };
 
   const responseOptions = responseOptionsLookup[location.pathname] || chatData.defaultResponses;
   const manualResponses = chatData.manualResponses;
 
   useEffect(() => {
-    if (!location.pathname.includes("/intro-to-")) {
+      if (!location.pathname.includes("/intro-to-") && !location.pathname.includes("/intermediate-")) {
       setIsOpen(false);
     }
   }, [location.pathname]);
@@ -43,10 +75,15 @@ const Chatbot = () => {
 
     const botResponse = manualResponses[userInput] || manualResponses.default;
 
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { text: botResponse, fromUser: false },
-    ]);
+    setBotIsTyping(true);
+
+    setTimeout(() => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: botResponse, fromUser: false },
+      ]);
+      setBotIsTyping(false);
+    }, 1000);
   };
 
   const handleToggleChatbot = () => {
@@ -80,6 +117,9 @@ const Chatbot = () => {
                 {message.text}
               </div>
             ))}
+            {botIsTyping && (
+              <div className="bot-message">💬</div>
+            )}
             <div ref={messagesEndRef} />
           </div>
           <div className="response-box">
